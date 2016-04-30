bdbe2f9ceff17c1228dee5b46df05abcee2962b4
Aadesh Maheshwari <TailoredTech@Aadeshs-MacBook-Pro-2.local>
2016-04-30 13:06:56 +0530

    upload project

---
+++ Panorama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Panorama
+//
+//  Created by Aadesh Maheshwari on 30/04/16.
+//  Copyright © 2016 Aadi007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
